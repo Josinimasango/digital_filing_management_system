a2185f4eb3688f88be05207f0b739ac8392d7490
Josinimasango <nellyjossydumako@gmail.com>
2024-03-21 16:24:14 +0200

    updating settings

--- records_management/settings.py
+++ records_management/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'records_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql',
         'NAME':'filing',
         'USER':'postgres',
         'PASSWORD':'Nelisiwe@94',
