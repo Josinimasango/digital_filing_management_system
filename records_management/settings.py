bec1879d942d45ded180b97d445ac5200736b611
Josinimasango <nellyjossydumako@gmail.com>
2024-03-22 12:43:28 +0200

    updating settings

--- records_management/settings.py
+++ records_management/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import media
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-)1=ql_obkn@xu6zxrvr4qxlzn@c%&a++n7=c(mau+do!3a9!oh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['digital-filing-management-system.vercel.app','.now.sh', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh', 'localhost', '127.0.0.1']
 
 
 # Application definition
