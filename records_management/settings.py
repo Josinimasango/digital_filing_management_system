a69b759127da8db1c3b89f2150e25d3e1272f83b
Josinimasango <nellyjossydumako@gmail.com>
2024-05-17 15:09:30 +0200

    updating settings

--- records_management/settings.py
+++ records_management/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 import django_heroku
 from pathlib import Path
-import media
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
