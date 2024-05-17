0afa22d22ff23166adc1d89cd68236df63834714
Josinimasango <nellyjossydumako@gmail.com>
2024-05-17 16:08:29 +0200

    modify

--- patient/models.py
+++ patient/models.py
@@ -10,9 +10,12 @@ class Patient(models.Model):
     name = models.CharField(max_length=200)
     age = models.IntegerField(default=0)
     gender = models.CharField(choices=GENDER_CHOICES, default='male', max_length=10)
-    mobile = models.IntegerField(null=True, blank=True)
+    mobile = models.BigIntegerField(null=True, blank=True)
     address = models.TextField(null=True, blank=True)
     detail = models.TextField(null=True, blank=True)
     medicine_detail = models.TextField(null=True, blank=True)
     note = models.TextField(null=True, blank=True)
     next_visit = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
